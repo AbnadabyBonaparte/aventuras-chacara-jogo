44f90499aaf427aa794fdfce6b3fad2338b48aff
AbnadabyBonaparte <abnadabybonaparte@gmail.com>
2025-07-28 19:13:59 -0300

    Create Apache.jsx

---
+++ src/components/pets/Apache.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Pet from './Pet';
+
+const Apache = ({ playerPosition }) => {
+  return <Pet type="apache" playerPosition={playerPosition} />;
+};
+
+export default Apache;
