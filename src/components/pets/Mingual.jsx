df6a10d7eb9adfa11f9445ae43ff482320361d4f
AbnadabyBonaparte <abnadabybonaparte@gmail.com>
2025-07-28 19:14:20 -0300

    Create Mingual.jsx

---
+++ src/components/pets/Mingual.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Pet from './Pet';
+
+const Mingual = ({ playerPosition }) => {
+  return <Pet type="mingual" playerPosition={playerPosition} />;
+};
+
+export default Mingual;
