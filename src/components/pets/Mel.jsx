07a8ac66b2f7a2d5f0602b790ce81481214e9b85
AbnadabyBonaparte <abnadabybonaparte@gmail.com>
2025-07-28 19:13:27 -0300

    Create Mel.jsx

---
+++ src/components/pets/Mel.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Pet from './Pet';
+
+const Mel = ({ playerPosition }) => {
+  return <Pet type="mel" playerPosition={playerPosition} />;
+};
+
+export default Mel;
